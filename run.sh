ac07c6a90bf5d90710e47de8d33dd458405929db
Cédric Foellmi <cedric@onekiloparsec.dev>
2026-01-28 19:34:54 +0100

    Corrected image name
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker run --rm \
   -p 127.0.0.1:8900:8900 \
   -v arcsecond_astrometry_cache:/data/astrometry \
-  arcsecond-platsolver-astrometry:latest
+  arcsecond-service-platesolver-astrometry:latest
 
